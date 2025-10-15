a023c843cb0e433f0f9453a2a55e760084258806
Vigneshwar <162857763+Vigneshwarprakash@users.noreply.github.com>
2025-10-15 12:34:11 +0530

    Add files via upload

---
+++ simplifyPath.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] directories = path.split("/"); 
+        for (String dir : directories) { 
+            if (dir.equals(".") || dir.isEmpty()) { 
+            } else if (dir.equals("..")) { 
+                if (!stack.isEmpty()) { 
+                    stack.pop();
+                }
+            } else { 
+                stack.push(dir);
+            }
+        }
+        return "/" + String.join("/", stack); 
+    }
+}
